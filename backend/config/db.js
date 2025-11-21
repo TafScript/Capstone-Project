9563acee5ef2057ff5ec87e103362ba903a4564f
Tafari <Tafarigeorge5079@gmail.com>
2025-11-21 11:38:09 -0500

    User Schema added, might update later

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 const connectDB = async () => {
     try{
         const connect = await mongoose.connect(process.env.MONGO_URI)
+        console.log("Connected to MongoDB");
     } catch (err) {
         console.log(err)
     }
